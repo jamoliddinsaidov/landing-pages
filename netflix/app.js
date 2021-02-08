5257eb39282071bf55960322337ff5bcdd47599c
sjamoliddin <sjamolbek@mail.ru>
2021-02-08 16:25:48 +0500

    add netflix clone

---
+++ netflix/app.js
@@ -0,0 +1,16 @@
+// Selectors
+const questions = document.querySelectorAll('.question');
+const answers = document.querySelectorAll('.answer');
+
+// Event Listeners
+questions.forEach((question, indexQ) => {
+	question.addEventListener('click', () => {
+		answers.forEach((answer, indexA) => {
+			if (indexQ === indexA) {
+				answer.classList.toggle('active');
+			} else {
+				answer.classList.remove('active');
+			}
+		});
+	});
+});
